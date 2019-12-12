8b371365c44c917c1c24c5056050d68abf23e3e8
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-12-12 12:07:42 -0300

    Cart styles

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -9,10 +9,21 @@ import {
   EmptyText,
   Products,
   Product,
+  ProductBody,
   ProductImage,
   ProductDetails,
   ProductTitle,
   ProductPrice,
+  ProductDelete,
+  ProductFooter,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  Total,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
 } from './styles';
 
 function Cart({ cart }) {
@@ -22,13 +33,39 @@ function Cart({ cart }) {
         <Products>
           {cart.map(product => (
             <Product key={product.id}>
-              <ProductImage source={{ uri: product.image }} />
-              <ProductDetails>
-                <ProductTitle>{product.title}</ProductTitle>
-                <ProductPrice>{product.price}</ProductPrice>
-              </ProductDetails>
+              <ProductBody>
+                <ProductImage source={{ uri: product.image }} />
+                <ProductDetails>
+                  <ProductTitle>{product.title}</ProductTitle>
+                  <ProductPrice>{product.price}</ProductPrice>
+                </ProductDetails>
+                <ProductDelete>
+                  <Icon name="delete-forever" size={24} color="#7159c1" />
+                </ProductDelete>
+              </ProductBody>
+              <ProductFooter>
+                <ProductControlButton>
+                  <Icon
+                    name="remove-circle-outline"
+                    color="#7159c1"
+                    size={22}
+                  />
+                </ProductControlButton>
+                <ProductAmount>10</ProductAmount>
+                <ProductControlButton>
+                  <Icon name="add-circle-outline" color="#7159c1" size={22} />
+                </ProductControlButton>
+                <ProductSubtotal>123</ProductSubtotal>
+              </ProductFooter>
             </Product>
           ))}
+          <Total>
+            <TotalText>Total</TotalText>
+            <TotalAmount>1234</TotalAmount>
+            <Order>
+              <OrderText>Finalizar Pedido</OrderText>
+            </Order>
+          </Total>
         </Products>
       ) : (
         <EmptyContainer>
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  padding: 10px;
+  padding: 5px;
   background: #fff;
   border-radius: 4px;
   margin: 15px;
@@ -9,14 +9,19 @@ export const Container = styled.View`
 
 export const Products = styled.View``;
 
-export const Product = styled.View`
+export const Product = styled.View``;
+
+export const ProductBody = styled.View`
   flex-direction: row;
+  /* border: 1px solid #eee;
+  border-radius: 4px; */
+  padding: 10px;
 `;
 
 export const ProductDetails = styled.View`
   justify-content: space-around;
   flex: 1;
-  margin-left: 10px;
+  margin-left: 12px;
 `;
 
 export const ProductImage = styled.Image`
@@ -26,13 +31,71 @@ export const ProductImage = styled.Image`
 
 export const ProductTitle = styled.Text`
   font-size: 14px;
-  color: #333333;
-  line-height: 18px;
-`;
-export const ProductPrice = styled.Text`
-  font-size: 16px;
   line-height: 19px;
+`;
+
+export const ProductPrice = styled.Text`
   font-weight: bold;
+  font-size: 16px;
+`;
+
+export const ProductDelete = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductFooter = styled.View`
+  flex-direction: row;
+  background: #eee;
+  padding: 15px;
+  align-items: center;
+`;
+
+export const ProductControlButton = styled.TouchableOpacity``;
+
+export const ProductAmount = styled.Text`
+  margin: 0 10px;
+  background: #fff;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export const ProductSubtotal = styled.Text`
+  text-align: right;
+  flex: 1;
+  font-weight: bold;
+`;
+
+export const Total = styled.View`
+  margin-top: 20px;
+  align-items: center;
+`;
+
+export const TotalText = styled.Text`
+  font-size: 20px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #999999;
+`;
+export const TotalAmount = styled.Text`
+  margin: 10px 0;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+export const Order = styled.TouchableOpacity`
+  background: #7159c1;
+  width: 100%;
+  border-radius: 4px;
+`;
+
+export const OrderText = styled.Text`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 18px;
+  padding: 12px;
+  text-align: center;
+  color: #fff;
 `;
 
 export const EmptyContainer = styled.View`
